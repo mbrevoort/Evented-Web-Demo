e9a20c81e6b7d4182bdaad64e46148d8274ccf71
Mike Brevoort <mike@brevoort.com>
2011-03-30 21:08:07 -0600

    added express, still playing

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var http = require('http');
- 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-      res.end("Hello world\n");
-});
- 
-server.listen(process.env.PORT || 8001);
+var http      = require('http'),
+    express   = require('express'),
+    hbs         = require('hbs'),
+    app       = express.createServer();
 
+app.configure(function(){
+  app.set("view engine", "hbs");
+  app.set('views', __dirname + '/views');
+  app.use(express.static(__dirname + '/static'));
+});  
+
+app.get('/', function(req, res, next) {
+  res.end("hmmmm what to do with this...\n");
+});
+
+app.listen(process.env.PORT || 8001);
