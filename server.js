b2ddf55d80bf22d447d5f400316b50e45705434d
Mike Brevoort <mike@brevoort.com>
2011-04-08 07:45:51 -0600

    Created branch remix

--- server.js
+++ server.js
@@ -13,5 +13,15 @@ app.get('/', function(req, res, next) {
   res.end("hmmmm what to do with this...\n");
 });
 
+// call twilio, record message, in a websocket stream recorded audio and transcription
+
+app.get('/twilio', function(res, res, next) {
+    res.render("twilio", { locals: { } });
+});
+
+app.get('/twilio_transcribe', function(res, res, next) {
+    res.render("twilio/transcribe", { locals: { } });    
+});
+
 app.listen(process.env.PORT || 8001);
 
