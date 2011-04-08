0c24bd98e982784e84ccd270d8f8d9d843dd29be
Mike Brevoort <mike@brevoort.com>
2011-04-08 07:50:10 -0600

    test

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var http      = require('http'),
-    express   = require('express'),
+var http        = require('http'),
+    express     = require('express'),
+    util        = require('util'),
     hbs         = require('hbs'),
-    app       = express.createServer();
+    app         = express.createServer();
 
 app.configure(function(){
   app.set("view engine", "hbs");
@@ -15,12 +16,13 @@ app.get('/', function(req, res, next) {
 
 // call twilio, record message, in a websocket stream recorded audio and transcription
 
-app.get('/twilio', function(res, res, next) {
+app.get('/twilio', function(req, res, next) {
     res.render("twilio", { locals: { } });
 });
 
-app.get('/twilio_transcribe', function(res, res, next) {
+app.get('/twilio_transcribe', function(req, res, next) {
     res.render("twilio/transcribe", { locals: { } });    
+    util.log(util.inspect(req));
 });
 
 app.listen(process.env.PORT || 8001);
