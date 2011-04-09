fd8769ccd4207aeaac80f34b5f10c98c4c97ef26
Mike Brevoort <mike@brevoort.com>
2011-04-09 16:00:06 -0600

    added status

--- server.js
+++ server.js
@@ -19,13 +19,26 @@ app.get('/', function(req, res, next) {
 // call twilio, record message, in a websocket stream recorded audio and transcription
 
 app.get('/twilio', function(req, res, next) {
-    console.log(req.query);
+
+    var message = {
+        type: "incoming",
+        data: req.query
+    };
+
+    socket.broadcast(JSON.stringify(message));
     res.render("twilio", { locals: { } });
 });
 
 app.post('/twilio', function(req, res, next) {
     var recUrl = req.body.RecordingUrl;
     util.log("Recording URL=" + recUrl);
+
+    var message = {
+        type: "recording",
+        data: req.body
+    };
+        
+    socket.broadcast(JSON.stringify(message));
     return res.send("ok");
 });
 
@@ -59,3 +72,30 @@ function broadcastStatus() {
     });
 }
 
+/*
+{ AccountSid: 'AC8d378f413f9f2742932bc8642a0d5c02',
+  ToZip: '80640',
+  FromState: 'CO',
+  Called: '+13033515939',
+  FromCountry: 'US',
+  CallerCountry: 'US',
+  CalledZip: '80640',
+  Direction: 'inbound',
+  FromCity: 'AURORA',
+  CalledCountry: 'US',
+  CallerState: 'CO',
+  CallSid: 'CA633b577f4fd02e215baf8f9682322ad6',
+  CalledState: 'CO',
+  From: '+13038342509',
+  CallerZip: '80012',
+  FromZip: '80012',
+  CallStatus: 'ringing',
+  ToCity: 'HENDERSON',
+  ToState: 'CO',
+  To: '+13033515939',
+  ToCountry: 'US',
+  CallerCity: 'AURORA',
+  ApiVersion: '2010-04-01',
+  Caller: '+13038342509',
+  CalledCity: 'HENDERSON' }
+*/
--- static/js/app.js
+++ static/js/app.js
@@ -10,6 +10,14 @@
         if(message.type === "status") {
             $("#num_clients span").html(message.num_clients);
         }
+        else if(message.type === "incoming") {
+            console.log(message.data);
+            incomingCall(message.data);
+        }
+        else if(message.type === "recording") {
+            console.log(message.data);
+            newRecording(message.data);
+        }
     });
     
     socket.on('disconnect', function(){
@@ -17,4 +25,15 @@
     });
     
     socket.connect();
+    
+    setInterval(function() { incomingCall({ CllSid: "12345", From: "3035551212", FromCity: "Aurora", FromState: "CO"})}, 5000);
+    
 // }(jQuery);
+
+function incomingCall(data) {
+    $('<div class="incoming ' + data.CallSid + '">incoming call<br/>from:<br/><br/>' + data.From + '<br/>' + data.FromCity + ', ' + data.FromState +'</div>').prependTo("#status_container");
+}
+
+function newRecording(data) {
+    $('.' + data.CallSid).append('<div><a href="' + data.RecordingUrl + '">Recording</a></div>');
+}
