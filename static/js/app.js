ed9c351564bd741b460f5c0a2a3a36024d3494eb
Mike Brevoort <mike@brevoort.com>
2011-04-09 16:04:31 -0600

    added recording audio tag

--- static/js/app.js
+++ static/js/app.js
@@ -35,5 +35,6 @@ function incomingCall(data) {
 }
 
 function newRecording(data) {
-    $('.' + data.CallSid).append('<div><a href="' + data.RecordingUrl + '">Recording</a></div>');
+    //http://api.twilio.com/2010-04-01/Accounts/AC8d378f413f9f2742932bc8642a0d5c02/Recordings/RE5d9cbb8101b8ac10ea6108ecb919c26e
+    $('.' + data.CallSid).append('<div><audio src="' + data.RecordingUrl + '" autoplay="true" controls preload="auto" autobuffer></audio></div>');
 }
